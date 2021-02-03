2f6f60da8fda92b0be539d95edd20c6907f8cf89
Naeemns <59644914+Naeemns@users.noreply.github.com>
2021-02-03 15:58:47 +0530

    Sahil (#1)
    * footer
    
    * footer
    
    Co-authored-by: isahilsachdev <isahilsachdev00@gmail.com>

--- meetup/src/App.js
+++ meetup/src/App.js
@@ -1,8 +1,10 @@
+
 import './App.css';
 import { EventCategories } from './Components/EventCategories';
 import { NavBar } from './Components/NavBar';
 import { EventPage } from './Components/Pages/MainPage/EventPage';
 import { Routes } from './Routes/Routes';
+import { Footer } from './Components/Footer';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       {/* <EventCategories /> */}
       <EventPage />
       <Routes />
+    <Footer/>
     </div>
   );
 }
--- meetup/src/index.js
+++ meetup/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from "./Redux/store"
 
 
