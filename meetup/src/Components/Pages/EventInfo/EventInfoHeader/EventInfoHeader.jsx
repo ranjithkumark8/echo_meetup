75a72ba18a3451c7544dd4f320d68b095387891f
archana singh <archanasingh1643@gmail.com>
2021-02-03 11:19:30 +0530

    created Event Information componenets

---
+++ meetup/src/Components/Pages/EventInfo/EventInfoHeader/EventInfoHeader.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Styles from './EventInfoHeader.module.css'
+import {AiOutlineSearch} from "react-icons/ai";
+
+const EventInfoHeader = () => {
+    return (
+        <div>
+            <div className = {Styles.header}>
+                <div className = {Styles.eventInfo_left}>
+                    <img src = "https://secure.meetupstatic.com/s/img/5455565085016210254/logo/svg/logo--script.svg" alt = "logo" height = "70px" width = "100px"/>
+                    <div className = {Styles.EventInfoHeader__searchBar}>
+                        <AiOutlineSearch style = {{color : "grey"}}/>
+                        <h3 style = {{color : "#757575" , marginLeft : "20px"}}>Search</h3>
+                    </div>
+                </div>
+                
+                <div className = {Styles.eventInfo_right}>
+                    <div className = {Styles.eventInfoHeader__startAGroup}>Start a new group</div>
+                    <div className = {Styles.loginHeader__login_text}>Log in</div>
+                    <div className = {Styles.loginHeader__login_text}>Sign up</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export {EventInfoHeader}
