65b8ad19e078734470e59cd783aceeb459b1e7a5
Naeemns <59644914+Naeemns@users.noreply.github.com>
2021-02-04 18:46:32 +0530

    login, signup and categories (#6)

--- meetup/src/Components/Pages/Login/LoginForm.jsx
+++ meetup/src/Components/Pages/Login/LoginForm.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Styles from './LoginForm.module.css'
 import { FaFacebookF } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { DiApple } from "react-icons/di";
-import { v4 as uuid } from 'uuid';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getLoginDetails, loginFailure, loginSuccess } from '../../../Redux/AuthRedux/action';
 
 const init = {
     email: "",
-    password: ""
+    password: "",
+    keepSigned: false
 }
 const LoginForm = () => {
     const [loginData, setLoginData] = React.useState(init)
-    const { email, password } = loginData;
     const history = useHistory();
+    const dispatch = useDispatch();
+    const isError = useSelector(state => state.authReducer.isError);
+    const loginDetails = useSelector(state => state.authReducer.loginDetails);
+    const [wrongCredentials, setWrongCredentials] = useState(false);
 
     const handleChange = (e) => {
         const { name, value, type, checked } = e.target;
@@ -23,28 +28,41 @@ const LoginForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const updatedLoginData = {
-            id: uuid(),
-            ...loginData
-        }
-        let data = JSON.parse(localStorage.getItem("loginInfo")) || []
-        data.push(updatedLoginData)
-        localStorage.setItem("loginInfo", JSON.stringify(data))
-        history.push("/home");
+        dispatch(getLoginDetails(loginData))
     }
+
+    const handleCheck = () => {
+        if (loginDetails.password === loginData.password) {
+            dispatch(loginSuccess());
+            history.push("/home");
+        } else {
+            dispatch(loginFailure())
+            setWrongCredentials(true);
+        }
+    }
+
+    useEffect(() => {
+        if (Object.keys(loginDetails).length !== 0) {
+            handleCheck()
+        }
+    }, [loginDetails])
+
+
+    const { email, password, keepSigned } = loginData;
 
     return (
         <div className={Styles.Login__loginForm}>
             <h2 className={Styles.loginform__heading}>Login</h2>
-            <p className={Styles.loginform__para}>Not registered with us yet? Sign up</p>
+            <p className={Styles.loginform__para}>Not registered with us yet? <Link to="/register">Sign up</Link></p>
             <div className={Styles.login_underline}></div>
+            {(wrongCredentials || isError) && <div style={{ color: 'red' }}>Email or password is wrong</div>}
             <form onSubmit={handleSubmit}>
                 <label className={Styles.login__label}>Email Address</label>
-                <input className={Styles.loginform__input} type="text" name="email" value={email} onChange={handleChange} />
+                <input className={Styles.loginform__input} type="text" name="email" value={email} onChange={handleChange} required />
                 <label className={Styles.login__label}>Password</label>
                 <div style={{ fontSize: "13px", fontWeight: "600", marginTop: "5px", color: "#00a2c7" }}  >Forgot your password ? </div>
-                <input className={Styles.loginform__input} type="password" name="password" value={password} onChange={handleChange} />
-                <input type="checkbox" name="keepSigned" checked="keepSigned" />
+                <input className={Styles.loginform__input} type="password" name="password" value={password} onChange={handleChange} required />
+                <input type="checkbox" name="keepSigned" checked={keepSigned} onChange={handleChange} />
                 <label className={Styles.login__label} style={{ marginLeft: "15px" }}>Keep me signed in</label><br></br>
                 <input className={Styles.login__login_button} type="submit" value="Log In" />
                 <div className={Styles.login__social_div}>
